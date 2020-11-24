97cecf3f78870f2b04c9238785e6923affc74f66
Phil McDaniel <mcdanipc@gmail.com>
2020-11-24 13:48:00 -0600

    first section from while loops added

--- 7_user_input_and_while_loops.py
+++ 7_user_input_and_while_loops.py
@@ -45,5 +45,16 @@ if number % 2 == 0:
 else:
     print(f"\nThe number {number} is odd")
 
-
 #while loops
+current_number = 1
+while current_number <= 5:
+        print(current_number)
+        current_number += 1
+
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program"
+
+message = ""
+while message != "quit":
+        message = input(prompt)
+        print(message)

1f5d57af87256d7d59c17005cbecd5b5b48a16fb
Phil McDaniel <mcdanipc@gmail.com>
2021-08-18 17:20:41 -0500

    finish chapter 10
    finish chapter 10

--- 10_files_and_exceptions.py
+++ 10_files_and_exceptions.py
@@ -94,3 +94,158 @@ while True:
     #else runs with success of try block
     else:
         print(answer)
+
+#handling FileNotFoundError exception
+filename = 'alice.txt'
+try:
+    with open(filename,encoding='utf-8') as f:
+        contents = f.read()
+except FileNotFoundError:
+    print(f'sorry the file {filename} not exist')
+
+title = 'Alice In Wonderland'
+title.split(' ')
+
+#wordcount
+filename = '10_alice.txt'
+try:
+    with open(filename,'r',encoding='utf-8') as f:
+        contents = f.read()
+except:
+    print('file does not exist')
+else:
+    words = contents.split(" ")
+    num_words = len(words)
+    print(f"{filename} has around {num_words} words")
+
+#working with multiple files
+def count_words(filename):
+    """count the approximate number of words in a file."""
+    try:
+        with open(filename,'r',encoding='utf-8') as f:
+            contents = f.read()
+    except:
+        print('file does not exist')
+    else:
+        words = contents.split(" ")
+        num_words = len(words)
+        print(f"{filename} has around {num_words} words")
+
+filename = '10_alice.txt'
+count_words(filename)
+
+filenames = ['10_alice.txt','10_siddhartha.txt','10_moby_dick.txt','10_little_women.txt']
+for file in filenames:
+    count_words(file)
+
+
+#storing data
+#json is a good option for storing data
+
+import json
+numbers = [2,3,5,7,11,13]
+
+filename = '10_numbers.json'
+with open(filename,'w') as f:
+    json.dump(numbers,f)
+
+with open(filename,'r') as f:
+    numbers=json.load(f)
+print(numbers)
+
+#saving and reading user-generated data
+username = input('What is your name?')
+
+#store data from input
+filename = '10_username.json'
+with open(filename,'w') as f:
+    json.dump(username,f)
+    print(f"We'll remember you when you come back, {username}")
+
+#load data that was stored
+with open(filename) as f:
+    username = json.load(f)
+    print(f'welcome back, {username}')
+
+
+#load stored data if exists, else prompt for value
+filename = '10_username.json'
+try:
+    with open(filename,'r') as f:
+        username = json.load(f)
+except FileNotFoundError:
+    username = input('What is your name?')
+    with open(filename,'w') as f:
+        json.dump(username,f)
+        print(f"we'll remember you when you comeback, {username}")
+
+#refactor by puting in function
+
+def greet_user():
+    """greet the user by name"""
+    filename = '10_username.json'
+    try:
+        with open(filename,'r') as f:
+            username = json.load(f)
+    except FileNotFoundError:
+        username = input('What is your name?')
+        with open(filename,'w') as f:
+            json.dump(username,f)
+            print("we'll remember you when you come back, {username}")
+    else:
+        print(f'welcome back {username}')
+greet_user()
+
+def get_stored_username():
+    """Get stored username, if available"""
+    filename = '10_username.json'
+    try:
+        with open(filename,'r') as f:
+            username = json.load(f)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
+
+def greet_user():
+    """greet the user by name"""
+    username = get_stored_username()
+    if username:
+        print(f"welcome back {username}")
+    else:
+        username = input('Whats your name?')
+        filename = '10_username.json'
+        with open(filename,'w') as f:
+            json.dump(filename,f)
+            print(f"we'll remember you {username}")
+
+greet_user()
+
+#all pieces inside functions
+def get_stored_username():
+    """Get stored username, if available"""
+    filename = '10_username.json'
+    try:
+        with open(filename,'r') as f:
+            username = json.load(f)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
+def get_new_username():
+    """prompt for a new username"""
+    username = input("What is your name?")
+    filename = '10_username.json'
+    with open(filename,'w') as f:
+        json.dump(username,f)
+    return username
+
+def greet_user():
+    """greet the user by name"""
+    username = get_stored_username()
+    if username:
+        print(f"welcome back {username}")
+    else:
+        username = get_new_username()
+        print(f"we'll remember you when you come back {username}")
+greet_user()

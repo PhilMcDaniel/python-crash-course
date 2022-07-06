4753988d3bd33ff4bd83a1189775c2ad14acd5ee
Phil McDaniel <mcdanipc@gmail.com>
2022-07-06 13:28:09 -0500

    Add move left function and refactor
    Add move left function and refactor

--- 12 Alien Invasion/alien_invasion.py
+++ 12 Alien Invasion/alien_invasion.py
@@ -35,9 +35,13 @@ class AlienInvasion:
                 if event.key == pygame.K_RIGHT:
                     #move the image to the right
                     self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = False
+                elif event.key ==pygame.K_LEFT:
+                    self.ship.moving_left = False
 
     def _update_screen(self):
         """Update images on the screen and flip to the new screen"""
--- 12 Alien Invasion/ship.py
+++ 12 Alien Invasion/ship.py
@@ -6,6 +6,7 @@ class Ship:
     def __init__(self,ai_game):
         """Initialize the ship and its starting position"""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         #load the ship image and get its rect
@@ -15,13 +16,22 @@ class Ship:
         #start ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # store decimal value for ships horizontal position
+        self.x = float(self.rect.x)
+
         #movement flag
         self.moving_right = False
+        self.moving_left = False
 
     def update(self):
         """update ship position based on movement flag"""
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
+        if self.moving_left:
+            self.x -= self.settings.ship_speed
+
+        #update rect from self.x
+        self.rect.x = self.x
 
     def blitme(self):
         """draw the ship at its current location."""
